f4c61529fe407317ea706fcd3b8e4036da5c34a4
FedeGioz <63424787+FedeGioz@users.noreply.github.com>
2025-01-16 13:30:01 +0100

    Created Cat model (#1)
    Co-authored-by: Stefano Ristorto <stefano.ristorto@itiscuneo.eu>

---
+++ CatStore/models.py
@@ -0,0 +1,97 @@
+from enum import Enum
+
+from django.db import models
+from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class Cat(models.Model):
+    def __init__(self):
+        self.id = 0
+        self.name = ""
+        self.age = 0
+        self.color = ""
+        self.breed = ""
+        self.price = 0
+        self.image_url = ""
+        self.description = ""
+
+    @property
+    def id(self):
+        return self.id
+
+    @id.setter
+    def id(self, value):
+        self.id = value
+
+    @property
+    def name(self):
+        return self.name
+
+    @name.setter
+    def name(self, value):
+        if not value:
+            raise ValueError("Name cannot be null")
+        self.name = value
+
+    @property
+    def age(self):
+        return self.age
+
+    @age.setter
+    def age(self, value):
+        self.age = value
+
+    @property
+    def color(self):
+        return self.color
+
+    @color.setter
+    def color(self, value):
+        self.color = value
+
+    @property
+    def breed(self):
+        return self.breed
+
+    @breed.setter
+    def breed(self, value):
+        self.breed = value
+
+    @property
+    def price(self):
+        return self.price
+
+    @price.setter
+    def price(self, value):
+        self.price = value
+
+    @property
+    def image_url(self):
+        return self.imageUrl
+
+    @image_url.setter
+    def image_url(self, value):
+        self.imageUrl = value
+
+    @property
+    def description(self):
+        return self.description
+
+    @description.setter
+    def description(self, value):
+        self.description = value
+
+
+class Color(Enum):
+    UNDEFINED = 0
+    BLACK = 1
+    WHITE = 2
+    BROWN = 3
+    GRAY = 4
+    ORANGE = 5
+    YELLOW = 6
+    MIXED = 6
+
+
--- CatStore/views.py
+++ CatStore/views.py
@@ -5,6 +5,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import *
 
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 def index(request):
     return render(request, 'master.html')
