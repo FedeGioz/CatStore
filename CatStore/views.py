a7a1f6025a5c2987ada34bc77a46675ce2fb4622
fedegioz <fedechn26@gmail.com>
2025-01-16 13:10:16 +0100

    Added static files configuration and a sample master page

---
+++ CatStore/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import *
+
+
+def index(request):
+    return render(request, 'master.html')
